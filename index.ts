7e4c5031c78df33b41ec334e1babe84e7c02f5d7
alethetwin <alethetwin@icloud.com>
2022-05-28 13:03:42 -0500

    export endponits to file

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import app from "./app";
-
+import { writeFileSync } from "fs";
 const port = 8080;
+
+const listEndpoints = require("express-list-endpoints"); // npm i express-list-endpoints
+let listaDeRutas = listEndpoints(app);
+
+writeFileSync("./routes.json", JSON.stringify(listaDeRutas));
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
